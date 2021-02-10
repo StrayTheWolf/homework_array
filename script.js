1eb71b547b380ea545ec09dc0a2b5ee8abd3f249
Wolf Stanislav <ork2004@yandex.ru>
2021-02-10 14:39:15 +0300

    added filter array function

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
 'use strict'
 
-// сортировка массива
+// sort
 
-function sort (arr){
-    for (let i = 0; i < arr.length;i++)
+function sort (array){
+    for (let i = 0; i < array.length;i++)
         for (let j = 0; j < i; j++)
-            if (arr[i] < arr[j]){
-                let tempArray = arr[i];
-                arr[i] = arr[j];
-                arr[j] = tempArray;
+            if (array[i] < array[j]){
+                let tempArray = array[i];
+                array[i] = array[j];
+                array[j] = tempArray;
             }
-    return arr;
+    return array;
 }
-let sortArray = sort([1,5,3,6,8,7,10,9,2,4])
+const sortArray = sort([1,5,3,6,8,7,10,9,2,4])
 console.log(sortArray)
 
-// сортировка массива
 
+// map
 
-
-// фильтрация массива
-
-function filter(){
-
+function map (array, callback){
+    for (let i = 0; i < array.length; i++){
+        array[i] = callback(array[i]);
+    }
+    return array;
 }
+const mapArray = map([1,2,3,4,5,6,7,8,9,10],(value)=>{
+    return value * 2;
+})
+console.log(mapArray);
+
+//filter
+
+function filter (array, callback) {
+    let tempArray = [];
+    let resultArray =[];
+    for(let i = 0; i < array.length; i++){
+        tempArray[i] = callback(array[i]);
+    }
+    for (let i = 0; i < array.length; i++){
+        if(tempArray[i] !== undefined){
+            resultArray.push(tempArray[i]);
+        }
+    }
+    return resultArray;
+}
+
+const filteredArray = filter([2,15,5,7,16,20,1],(callback)=>{
+    if(callback > 10){
+        return callback;
+    }
+})
+console.log(filteredArray);
